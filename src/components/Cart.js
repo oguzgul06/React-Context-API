160805ae4e6c05d36cf146ac93fa53862d4f0a1a
oguz gül <ogzgl95@outlook.com>
2021-07-27 22:14:05 +0300

    useContext add to cart.js component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { BooksContext } from "../App";
 
 const Cart = () => {
+  const context = useContext(BooksContext);
+
   return (
     <div>
       <h2>
-        <Link to="/">Kitap Listesi</Link> <span>Sepetim</span>
+        <Link to="/">Book List</Link> <span>My Cart</span>
       </h2>
 
-      <h3>Toplam Sepet Tutarı: &#8378;19.99</h3>
+      <h3>Total Price: &#8378;19.99</h3>
 
-      <div className="book">
+      {context.state.cart.map((book) => (
+        <div className="book">
+          <img src={book.image} alt={book.name} />
+          <div>
+            <h4>{book.name}</h4>
+            <p>Author: {book.author}</p>
+            <p>Price: &#8378;{book.price}</p>
+            <p>Total Price: &#8378;{book.price * book.count}</p>
+            <p>You have {book.count} books in the your cart</p>
+            <button>-</button>
+            <button>Out of the cart</button>
+            <button>+</button>
+          </div>
+        </div>
+      ))}
+
+      {/*  <div className="book">
         <img
           src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
           alt="Simyacı"
@@ -25,7 +44,7 @@ const Cart = () => {
           <button>Sepetten Çıkar</button>
           <button>+</button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
