0314a2c78e3a049eb88731b72828355a3d18888e
oguz gül <ogzgl95@outlook.com>
2021-07-27 21:00:36 +0300

    Modified App.js and index.js and then add to data.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from "react";
+import "./styles.css";
+import { Route } from "react-router-dom";
 
-import './App.css';
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
-function App() {
+import { data } from "./data";
+
+export default function App() {
   return (
     <div className="App">
-     
+      <h1>
+        Shopping Cart
+        <img
+          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+          alt="React Dersleri"
+        />{" "}
+      </h1>
+      <Route exact path="/" component={Products} />
+      <Route path="/cart" component={Cart} />
     </div>
   );
 }
-
-export default App;
